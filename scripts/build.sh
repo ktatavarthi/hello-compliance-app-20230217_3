6c4242e38f933ae0cad650fe1e0acaf06c2d5081
Edina Pap <Edina.Pap@ibm.com>
2021-08-10 13:05:31 +0200

    wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -29,7 +29,7 @@ echo -n "$DIGEST" > ../image-digest
 echo -n "$IMAGE_TAG" > ../image-tags
 echo -n "$IMAGE" > ../image
 
-IMAGE_TAG_XRAY="eu.artifactory.swg-devops.com/wcp-compliance-automation-team-docker-local/""$IMAGE_NAME":"$TEMP_TAG"
+IMAGE_TAG_XRAY="eu.artifactory.swg-devops.com/wcp-compliance-automation-team-docker-local/$IMAGE_NAME"
 docker login wcp-compliance-automation-team-docker-local.artifactory.swg-devops.com -u "$ARTIFACTORY_USER" --password-stdin < /tmp/artifactory-token
 
 docker tag "$IMAGE" "$IMAGE_TAG_XRAY"
