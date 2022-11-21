394f8250f2b1067616f01493d5b653adb66c44a4
akshay vijapur <akshaykumar.vijapur@ibm.com>
2022-11-20 22:23:33 -0800

    feat: update required peer review approvers to 2

--- scripts/code_setup.sh
+++ scripts/code_setup.sh
@@ -6,6 +6,8 @@ source "${ONE_PIPELINE_PATH}"/tools/get_repo_params
 APP_TOKEN_PATH="./app-token"
 read -r APP_REPO_NAME APP_REPO_OWNER APP_SCM_TYPE APP_API_URL < <(get_repo_params "$(get_env APP_REPO)" "$APP_TOKEN_PATH")
 
+PEER_REVIEW_APPROVERS=$(get_env peer_review_approvers "1")
+
 if [[ $APP_SCM_TYPE == "gitlab" ]]; then
   curl --location --request PUT "${APP_API_URL}/projects/${APP_REPO_OWNER}%2F${APP_REPO_NAME}/" \
     --header "PRIVATE-TOKEN: $(cat $APP_TOKEN_PATH)" \
@@ -15,6 +17,6 @@ if [[ $APP_SCM_TYPE == "gitlab" ]]; then
     }'
 else
   curl -H "Authorization: Bearer $(cat ${APP_TOKEN_PATH})" "${APP_API_URL}/repos/${APP_REPO_OWNER}/${APP_REPO_NAME}/branches/master/protection" \
-    -XPUT -d '{"required_pull_request_reviews":{"dismiss_stale_reviews":true},"required_status_checks":{"strict":true,"contexts":["tekton/code-branch-protection","tekton/code-unit-tests","tekton/code-cis-check","tekton/code-vulnerability-scan","tekton/code-detect-secrets"]},"enforce_admins":null,"restrictions":null}'
+    -XPUT -d "{\"required_pull_request_reviews\":{\"dismiss_stale_reviews\":true, \"required_approving_review_count\":${PEER_REVIEW_APPROVERS}},\"required_status_checks\":{\"strict\":true,\"contexts\":[\"tekton/code-branch-protection\",\"tekton/code-unit-tests\",\"tekton/code-cis-check\",\"tekton/code-vulnerability-scan\",\"tekton/code-detect-secrets\"]},\"enforce_admins\":null,\"restrictions\":null}"
 fi
 npm ci
